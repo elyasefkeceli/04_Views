3a6f42f926f9b9c3002c8b3de71310367593a7ef
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-03-27 04:52:16 +0300

    Add project files.

---
+++ 04_Views/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Ana Sayfa
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+        //Hakkımızda
+        public ActionResult About()
+        {
+            return View();
+        }
+        //Iletisim
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
